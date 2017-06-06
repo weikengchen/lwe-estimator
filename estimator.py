50c404a936317649246317fa5e4772fda748130a
Martin R. Albrecht <martinralbrecht@googlemail.com>
2017-06-06 14:04:49 +0100

    move logging to its own namespace

--- estimator.py
+++ estimator.py
@@ -39,22 +39,46 @@ import sage.crypto.lwe
 oo = PlusInfinity()
 
 
-# Logging
+class Logging:
+    """
+    Control level of detail being printed.
+    """
 
-plain_logger = logging.StreamHandler()
-plain_logger.setFormatter(logging.Formatter('%(message)s',))
+    plain_logger = logging.StreamHandler()
+    plain_logger.setFormatter(logging.Formatter('%(message)s',))
 
-detail_logger = logging.StreamHandler()
-detail_logger.setFormatter(logging.Formatter('%(levelname)s:%(name)s: %(message)s',))
+    detail_logger = logging.StreamHandler()
+    detail_logger.setFormatter(logging.Formatter('%(levelname)s:%(name)s: %(message)s',))
 
-logging.getLogger("estimator").handlers = [plain_logger]
-logging.getLogger("estimator").setLevel(logging.INFO)
+    logging.getLogger("estimator").handlers = [plain_logger]
+    logging.getLogger("estimator").setLevel(logging.INFO)
 
-loggers = ("binsearch", "repeat", "guess")
+    loggers = ("binsearch", "repeat", "guess")
 
-for logger in loggers:
-    logging.getLogger(logger).handlers = [detail_logger]
-    logging.getLogger(logger).setLevel(logging.INFO)
+    for logger in loggers:
+        logging.getLogger(logger).handlers = [detail_logger]
+        logging.getLogger(logger).setLevel(logging.INFO)
+
+    CRITICAL = logging.CRITICAL
+    ERROR = logging.CRITICAL
+    WARNING = logging.CRITICAL
+    INFO = logging.CRITICAL
+    DEBUG = logging.CRITICAL
+    NOTSET = logging.CRITICAL
+
+    @staticmethod
+    def set_level(lvl, loggers=None):
+        """Set logging level
+
+        :param lvl: one of `CRITICAL`, `ERROR`, `WARNING`, `INFO`, `DEBUG`, `NOTSET`
+        :param loggers: one of `Logging.loggers`, if `None` all loggers are used.
+
+        """
+        if loggers is None:
+            loggers = Logging.loggers
+
+        for logger in loggers:
+            logging.getLogger(logger).setLevel(lvl)
 
 
 # Utility Classes #
