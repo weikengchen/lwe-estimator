1898b8e66abb34880de5534da823f9e7eeffafbb
Ben <pbva168@live.rhul.ac.uk>
2020-03-20 10:12:28 +0000

    switch on [Alb17] for binary secrets

--- estimator.py
+++ estimator.py
@@ -3130,7 +3130,7 @@ def estimate_lwe(n, alpha=None, q=None, secret_distribution=True, m=oo, # noqa
             algorithms["dec"] = partial(primal_decode, reduction_cost_model=reduction_cost_model)
 
     if "dual" not in skip:
-        if SDis.is_ternary(secret_distribution):
+        if SDis.is_ternary(secret_distribution) or SDis.is_binary(secret_distribution):
             algorithms["dual"] = partial(drop_and_solve, dual_scale, reduction_cost_model=reduction_cost_model,
                                          postprocess=True)
         elif SDis.is_small(secret_distribution):
